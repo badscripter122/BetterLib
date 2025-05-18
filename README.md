b61645b9b04ce8af588c4c00e8f02f62a3b3397a
badscripter <shut@up.git>
2025-05-18 01:00:18 -0400

    untested code :D

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# a libary to fix the issue of the non "write once run anywhere" System.load function
-i made this becuse im lazy
+# A library to fix the issue of the non "write once run anywhere" System.load function
+I made this because im lazy
+
+
+## The libs are found in the users directory and in the subdirectory libs
---
+++ src/main/java/FluidStudios/BetterLib.java
@@ -0,0 +1,24 @@
+package FluidStudios;
+
+public class BetterLib {
+    public static void LoadLibrary(String lib) {
+        String userDir = System.getProperty("user.dir");
+        String os = System.getProperty("os.name").toLowerCase();
+
+        String fileType = "";
+
+        if (os.contains("win")) {
+            fileType = ".dll";
+        } else if (os.contains("mac")) {
+            fileType = ".dylib";
+        } else if (os.contains("linux")) {
+            fileType = ".so";
+        }
+
+        if(userDir != null) {
+            System.load(userDir + "/libs/" + lib + fileType);
+        } else {
+            System.err.println("Error loading library: " + lib);
+        }
+    }
+}
